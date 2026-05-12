c81ce26d12d79aea49c517b3cb6f8783319b7118
DESKTOP-9J07U8P\ADMIN <tiepjimmy@gmail.com>
2026-05-12 18:05:55 +0700

    edit compose

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,24 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors();
-  // app.enableCors({
-  //   // 1. Chỉ định chính xác Origin của Frontend (không dùng '*')
-  //   origin: 'http://localhost:5173',
-  //
-  //   // 2. Cho phép gửi kèm Cookie và Header Authorization
-  //   credentials: true,
-  //
-  //   // 3. Các phương thức bạn cho phép
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //
-  //   // 4. Các header bạn cho phép
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  // });
+  app.enableCors({
+    // 1. Chỉ định chính xác Origin của Frontend (không dùng '*')
+    origin: [
+      'http://171.254.94.233:8088',
+      'http://localhost:5173',
+      'chrome-extension://ffmoaapbnaedainohnlefihmdnpgalpb'
+    ],
+
+
+    // 2. Cho phép gửi kèm Cookie và Header Authorization
+    credentials: true,
+
+    // 3. Các phương thức bạn cho phép
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+
+    // 4. Các header bạn cho phép
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(3002);
 }
 bootstrap();
